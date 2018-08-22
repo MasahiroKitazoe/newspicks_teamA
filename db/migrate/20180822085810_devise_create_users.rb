103c8419e08d2ababeae5a0d4af3688be0d9d002
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-22 19:33:07 +0900

    create users_table

--- db/migrate/20180822085810_devise_create_users.rb
+++ db/migrate/20180822085810_devise_create_users.rb
@@ -46,8 +46,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    add_index :users, :first_name
-    add_index :users, :last_name
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
