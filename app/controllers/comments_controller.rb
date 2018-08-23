e908e0027c0a234200ab114f168c3d03bc55267c
kawakamir <rui.kawakami@gmail.com>
2018-08-23 12:24:53 +0900

    create comment-controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,36 @@
 class CommentsController < ApplicationController
+  before_action :set_pick
+
+  def create
+    @comment = @pick.comments.create(comment_params)
+    html = render_to_string partial: 'picks/comment', locals: { comment: @comment }
+    render :json => {:html => html}
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id).merge(pick_id: @pick.id)
+  end
+
+  def set_pick
+    @pick = Pick.find(params[:pick_id])
+  end
 end
