ebc8854d6a17ac30a9a847b3c85712333dcf2830
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-23 18:15:51 +0900

    add signup modal

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,7 +1,10 @@
 $(function() {
-  // 「アカウント登録」クリック時
-  $('#signup').on('click', function(e) {
-    // e.preventDefault();
-  })
+  // 「アカウント登録」クリック時、モーダルを表示
+  $('#signup-btn').on('click', function(e) {
+    e.preventDefault();
+    $('#signup').css("display", "block");
+  });
+
+  // 背景をクリックするとモーダルを終了
   // 「ログイン」クリック時
 });
