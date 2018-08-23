364322b919bc11f3210584630507bf0a003e7f49
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-23 19:21:40 +0900

    add close_modal_function

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -3,16 +3,29 @@ $(function() {
   $('#signup-btn').on('click', function(e) {
     e.preventDefault();
     $('#signup').css("display", "block");
+
+    // 背景をクリックするとモーダルを終了
+    $('.signup__overlay').on('click', function(e) {
+      console.log(e.target);
+      if (e.target.className == "signup__overlay") {
+        $('#signup').css("display", "none");
+      }
+    });
   });
 
-  // 背景をクリックするとモーダルを終了
 
   // 「ログイン」クリック時
   $('#signin-btn').on('click', function(e) {
     e.preventDefault();
     $('#signin').css("display", "block");
+
+    // 背景をクリックするとモーダルを終了
+    $('.signin__overlay').on('click', function(e) {
+      console.log(e.target);
+      if (e.target.className == "signin__overlay") {
+        $('#signin').css("display", "none");
+      }
   });
 
-  // 背景をクリックするとモーダルを終了
-
+  });
 });
