c1d2e77039befc6b8fc42d728ff2ce753a8a7858
kawakamir <rui.kawakami@gmail.com>
2018-08-25 15:14:40 +0900

    stack dont fire js

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,6 +1,14 @@
 $(function(){
-  $(document).on("click", ".comment-container" function() {
+  $('.side-bar__my-news').on("click", function() {
     console.log('hello')
-    $(".comment-container").fadeOut();
-  });
-})
+  })
+  $('.drop-down-button').on("click", function() {
+    console.log('hello!')
+  })
+  $(".drop-down-button-1").on("click", function() {
+    console.log('hello!')
+  })
+  $("#comment-space").on("click", function(){
+    console.log('hello!');
+  })
+});
