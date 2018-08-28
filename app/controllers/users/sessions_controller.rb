9b51cf344e384ca0946b50968beb3d60cbee0a35
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-28 20:36:46 +0900

    seccess login redirect

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  include AjaxHelper
 
   # GET /resource/sign_in
   def new
@@ -10,7 +11,18 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    super
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    respond_to do |format|
+      if user_signed_in?
+        yield resource if block_given?
+        format.js { render ajax_redirect_to(root_path) }
+      else
+        @msg = resource.errors.full_messages
+        format.js
+      end
+    end
   end
 
   # DELETE /resource/sign_out
@@ -19,9 +31,8 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   protected
-
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  end
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
