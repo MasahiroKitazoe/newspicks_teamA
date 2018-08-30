a0ddb0efca8fb3f5c2e6e0f5410382d66738da6e
kawakamir <rui.kawakami@gmail.com>
2018-08-30 16:00:25 +0900

    create view edit

--- app/assets/javascripts/showpick.js
+++ app/assets/javascripts/showpick.js
@@ -1,17 +1,33 @@
 $(function() {
+  // コメント入力時に画面を大きく
   $("#comment-space").focusin(function(e) {
+    e.preventDefault();
     $(this).css("width", "480px");
   });
+  // コメント入力から離れた場合は画面を小さく
   $("#comment-space").focusout(function(e) {
+    e.preventDefault();
     $(this).css("width", "284px");
   });
 
-  $(document).on('ajax:complete', '.new_comment', function(event, ajax, status){
-    var response;
-    response = $.parseJSON(ajax.responseText);
-    $('.followings').append(response.html);
-    $('form')[0].reset();
+  // current_userのコメント欄で横のボタンをクリックした場合に表示
+  $('.menu-btn').on('click', function(e){
+    e.preventDefault();
+    $('.popover').css('display', 'block');
   });
+  // current_userのコメント欄で横のボタンをクリックした場合に表示
+  // $('.news-comment').on('click', function(e) {
+  //   console.log('hello')
+  //   e.preventDefault();
+  //   $('.popover').css('display', 'none');
+  // });
+
+  // $(document).on('click touchend', function(event) {
+  //   if (!$(event.target).closest('.popover').length) {
+  //     console.log('hello');
+  //     $('.popover').css('display', 'none');
+  //   }
+  // });
 
   $('.side-bar__my-news').on("click", function() {
     console.log('hello')
