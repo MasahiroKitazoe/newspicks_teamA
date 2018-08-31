f9fa4ade0e9967a42b6a459041e6a4d868ba088c
北添真広 <sas.surfer0jonny@gmail.com>
2018-08-31 15:03:09 +0900

    add like & unlike method as an instance method of comment model

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,13 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = Comment.params[:comment_id]
+    @comment.do_like(current_user)
+  end
+
+  def destroy
+    @comment = Like.find(params[:id]).comment
+    @comment.undo_like(current_user)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,12 @@ class Comment < ApplicationRecord
   has_many :likes
   validates :user_id, presence: true
   validates :pick_id, presence: true
+
+  def do_like(user)
+    likes.create(user_id: user.id)
+  end
+
+  def undo_unlike(user)
+    likes.find_by(user_id: user.id).destroy
+  end
 end
