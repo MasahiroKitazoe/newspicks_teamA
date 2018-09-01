37e16d60a98367bb4c44b568763a71bedce926ba
北添真広 <sas.surfer0jonny@gmail.com>
2018-09-01 15:29:44 +0900

    config for s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,8 +44,8 @@ end
 set :default_env, {
   rbenv_root: "/usr/local/rbenv",
   path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
-  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID_NP"],
-  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY_NP"]
+  AWS_ACCESS_KEY_ID_NP: ENV["AWS_ACCESS_KEY_ID_NP"],
+  AWS_SECRET_ACCESS_KEY_NP: ENV["AWS_SECRET_ACCESS_KEY_NP"]
 }
 
 # secrets.yml用のシンボリックリンクを追加
