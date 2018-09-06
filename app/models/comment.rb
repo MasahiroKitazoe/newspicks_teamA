d2a70370f9ecf0222f62fb3da3404d50a28cf46b
北添真広 <sas.surfer0jonny@gmail.com>
2018-09-06 14:56:42 +0900

    modify comment ranking method and add test on it

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,18 +23,34 @@ class Comment < ApplicationRecord
     return like.id
   end
 
-  def self.rank_comment(limit_num)
-    #1週間以内のCommentのidを取得
-    target_ids = Comment.where(created_at: [7.days.ago..Time.now]).ids
+  def self.get_comment_id_within_one_week
+    Comment.where(created_at: [7.days.ago..Time.now]).ids
+  end
 
-    #Like数の多いコメントを取得
+  def self.get_popular_comments(target_ids, limit_num)
     ids = Like.group(:comment_id).where(comment_id: target_ids).order('count_comment_id DESC').limit(limit_num).count(:comment_id).keys
     ranking = ids.map { |id| Comment.find(id) }
+  end
+
+  def self.get_popular_comment_all_time(limit_num)
+    ids = Like.group(:comment_id).order('count_comment_id DESC').limit(limit_num).count(:comment_id).keys
+    ranking =  ids.map { |id| Comment.find(id) }
+    if ranking.length == 0
+      ranking = Comment.order('created_at DESC').limit(limit_num)
+    end
+    return ranking
+  end
+
+  def self.rank_comment(limit_num)
+    #1週間以内のCommentのidを取得
+    target_ids = get_comment_id_within_one_week
+
+    #Like数の多いコメントを取得
+    ranking = get_popular_comments(target_ids, limit_num)
 
     #該当commentが0件のとき、代わりに時期無制限でlikeの多いcommentを取得
-    if ranking.nil?
-      ids = Like.group(:comment_id).order('count_comment_id DESC').limit(limit_num).count(:comment_id).keys
-      ranking =  ids.map { |id| Comment.find(id).include(:user) }
+    if ranking.length == 0
+      ranking = get_popular_comment_all_time(limit_num)
     end
 
     return ranking
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,66 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'test on rank comments' do
-    
+    context 'when comments made past one week exists' do
+      before do
+        @comments = []
+        @comment = FactoryBot.create(:comment, created_at: 10.days.ago)
+        10.times { @comments << FactoryBot.create(:comment) }
+      end
+
+      it 'does not include outdated comments' do
+        target_ids = Comment.get_comment_id_within_one_week
+        expect(target_ids).to_not include(@comment.id)
+      end
+
+      it 'gets accurate length of comment id array' do
+        target_ids = Comment.get_comment_id_within_one_week
+        expect(target_ids.length).to eq 10
+      end
+
+      it 'gets popular comments within one week' do
+        3.times { FactoryBot.create(:like, comment_id: @comments[0].id) }
+        2.times { FactoryBot.create(:like, comment_id: @comments[1].id) }
+        FactoryBot.create(:like, comment_id: @comments[2].id)
+
+        target_ids = Comment.get_comment_id_within_one_week
+        ranking = Comment.get_popular_comments(target_ids, 10)
+        expect(ranking.length).to eq 3
+      end
+    end
+
+    context 'when comments made past one week do not exists' do
+      before do
+        @comments = []
+        10.times { @comments << FactoryBot.create(:comment, created_at: 10.days.ago) }
+      end
+
+      it 'does not detect comments within one week' do
+        target_ids = Comment.get_comment_id_within_one_week
+        expect(target_ids.length).to eq 0
+      end
+
+      it 'does not get updated popular comments' do
+        target_ids = Comment.get_comment_id_within_one_week
+        ranking = Comment.get_popular_comments(target_ids, 10)
+        expect(ranking.length).to eq 0
+      end
+
+      it 'gets popular comments from all time' do
+        3.times { FactoryBot.create(:like, comment_id: @comments[0].id) }
+        2.times { FactoryBot.create(:like, comment_id: @comments[1].id) }
+        FactoryBot.create(:like, comment_id: @comments[2].id)
+
+        ranking = Comment.get_popular_comment_all_time(5)
+        expect(ranking.length).to eq 3
+      end
+
+      context 'when no likes added' do
+        it 'gets newest comments when no updated and liked comments exists' do
+          ranking = Comment.get_popular_comment_all_time(10)
+          expect(ranking.length).to eq 10
+        end
+      end
+    end
   end
 end
