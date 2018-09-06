01bb44b72c84b61dbef7765ca34433b4895212f3
北添真広 <sas.surfer0jonny@gmail.com>
2018-09-06 16:38:13 +0900

    modify model files

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,6 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    @relationship = current_user.active_relationships.find_by(followed_id: params[:followed_id])
     respond_to do |format|
       format.html { redirect_to user }
       format.json
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -22,35 +22,35 @@ class Comment < ApplicationRecord
     return like.id
   end
 
-  def self.get_comment_id_within_one_week
-    Comment.where(created_at: [7.days.ago..Time.now]).ids
-  end
-
-  def self.get_popular_comments(target_ids, limit_num)
-    ids = Like.group(:comment_id).where(comment_id: target_ids).order('count_comment_id DESC').limit(limit_num).count(:comment_id).keys
-    ranking = ids.map { |id| Comment.find(id) }
-  end
-
-  def self.get_popular_comment_all_time(limit_num)
-    ids = Like.group(:comment_id).order('count_comment_id DESC').limit(limit_num).count(:comment_id).keys
-    ranking =  ids.map { |id| Comment.find(id) }
-    if ranking.length == 0
-      ranking = Comment.order('created_at DESC').limit(limit_num)
+  class << self
+    def get_comment_id_within_one_week
+      Comment.where(created_at: [7.days.ago..Time.now]).ids
     end
-    return ranking
-  end
 
-  def self.rank_comment(limit_num)
-    #1週間以内のCommentのidを取得
-    target_ids = get_comment_id_within_one_week
-
-    #Like数の多いコメントを取得
-    ranking = get_popular_comments(target_ids, limit_num)
-
-    #該当commentが0件のとき、代わりに時期無制限でlikeの多いcommentを取得
-    if ranking.length == 0
-      ranking = get_popular_comment_all_time(limit_num)
+    def get_popular_comments(target_ids, limit_num)
+      ids = Like.group(:comment_id).where(comment_id: target_ids).order('count_comment_id DESC').limit(limit_num).count(:comment_id).keys
+      ranking = ids.map { |id| Comment.find(id) }
     end
-    return ranking
+
+    def get_popular_comment_all_time(limit_num)
+      ids = Like.group(:comment_id).order('count_comment_id DESC').limit(limit_num).count(:comment_id).keys
+      ranking =  ids.map { |id| Comment.find(id) }
+      if ranking.length == 0
+        ranking = Comment.order('created_at DESC').limit(limit_num)
+      end
+    end
+
+    def rank_comment(limit_num)
+      #1週間以内のCommentのidを取得
+      target_ids = get_comment_id_within_one_week
+
+      #Like数の多いコメントを取得
+      ranking = get_popular_comments(target_ids, limit_num)
+
+      #該当commentが0件のとき、代わりに時期無制限でlikeの多いcommentを取得
+      if ranking.length == 0
+        ranking = get_popular_comment_all_time(limit_num)
+      end
+    end
   end
 end
