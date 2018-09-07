13ef2138b0b31c2fa384f48e62505912ca17f417
kawakamir <rui.kawakami@gmail.com>
2018-09-07 12:44:59 +0900

    delete the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,3 @@ CSV.foreach('db/picks.csv') do |row|
   Pick.create(:url => row[0], :image => row[1], :title => row[2], :body => row[3], :created_at => row[4], :updated_at => row[5], :source => row[6])
 end
 
-CSV.foreach('db/picks.csv') do |row|
-  PickUser.create()
