2ce750f300cf4c725db660ef39daa0eb72ca8a93
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-10 19:38:56 +0900

    success to appear search-result in modal

--- app/assets/javascripts/keyword.js
+++ app/assets/javascripts/keyword.js
@@ -2,16 +2,20 @@ $(function() {
   $('#keyword-class-news').on('click', function() {
     $(this).css("color", "#4277FB");
     $(this).css("border-bottom", "2px solid #4277FB");
+    $(this).css("font-weight", "bold");
     $('#keyword-class-comment').css("color", "#999");
     $('#keyword-class-comment').css("border-bottom", "2px solid #fff");
+    $('#keyword-class-comment').css("font-weight", "normal");
     $('.keyword-wrapper__form__bottom__result__comments').css("display", "none");
     $('.keyword-wrapper__form__bottom__result__picks').css("display", "block");
   });
   $('#keyword-class-comment').on('click', function() {
     $(this).css("color", "#4277FB");
     $(this).css("border-bottom", "2px solid #4277FB");
+    $(this).css("font-weight", "bold");
     $('#keyword-class-news').css("color", "#999");
     $('#keyword-class-news').css("border-bottom", "2px solid #fff");
+    $('#keyword-class-news').css("font-weight", "normal");
     $('.keyword-wrapper__form__bottom__result__picks').css("display", "none");
     $('.keyword-wrapper__form__bottom__result__comments').css("display", "block");
   });
