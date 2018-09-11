16578de715aa7af9023f87a42f9679a6f6645169
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-11 18:31:57 +0900

    success to save keyword

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,6 +1,7 @@
 class KeywordsController < ApplicationController
   def create
-    keyword = Keyword.new(keyword: params[:keyword], user_id: current_user.id)
+    keyword = Keyword.new(keyword: params[:keyword][:keyword], user_id: current_user.id)
     keyword.save
+    redirect_to "/users/#{current_user.id}/timeline"
   end
 end
