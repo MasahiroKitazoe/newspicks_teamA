3a29eccc92e870f59f03e081b9447eb2d140b803
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-11 19:07:17 +0900

    create errors_modal

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,7 +1,10 @@
 class KeywordsController < ApplicationController
   def create
-    keyword = Keyword.new(keyword: params[:keyword][:keyword], user_id: current_user.id)
-    keyword.save
-    redirect_to "/users/#{current_user.id}/timeline"
+    @keyword = Keyword.new(keyword: params[:keyword][:keyword], user_id: current_user.id)
+    if keyword.save
+      redirect_to "/users/#{current_user.id}/timeline"
+    else
+      binding.pry
+    end
   end
 end
