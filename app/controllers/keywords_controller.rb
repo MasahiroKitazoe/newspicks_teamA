0fcfe93b00c9b2beaa4aa07635018928f166a8da
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-11 19:40:38 +0900

    success set a junction done or false

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,10 +1,6 @@
 class KeywordsController < ApplicationController
   def create
     @keyword = Keyword.new(keyword: params[:keyword][:keyword], user_id: current_user.id)
-    if keyword.save
-      redirect_to "/users/#{current_user.id}/timeline"
-    else
-      binding.pry
-    end
+    @keyword.save
   end
 end
