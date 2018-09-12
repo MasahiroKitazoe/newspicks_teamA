4bf37c54423947a5225a71880cc7c9d4ba2f66d7
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-12 15:07:00 +0900

    success to appear keyword_delete_btn

--- app/assets/javascripts/keyword.js
+++ app/assets/javascripts/keyword.js
@@ -135,6 +135,7 @@ $(function() {
     $('.keyword-wrapper__form__above__input').css("display", "none");
     $('.keyword-wrapper__form__above__keyword').text(keyword);
     $('.keyword-wrapper__form__above__keyword').css("display", "block");
+    $('.keyword-wrapper__form__above__btn').css("display", "block");
     $('.keyword-wrapper__form__bottom__announce').css("display", "none");
     $('.keyword-wrapper__form__bottom__result').css("display", "block");
     $.ajax({
@@ -177,6 +178,7 @@ $(function() {
       if (e.target.className == "keyword-wrapper__overlay") {
         $('.keyword-wrapper').css("display", "none");
         $('.keyword-wrapper__form__above__keyword').css("display", "none");
+        $('.keyword-wrapper__form__above__btn').css("display", "none");
         $('.keyword-wrapper__form__above__input').css("display", "block");
         $('.keyword-wrapper__form__bottom__result').css("display", "none");
         $('.keyword-wrapper__form__bottom__announce').css("display", "block");
