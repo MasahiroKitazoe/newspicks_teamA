4c7b3064452f11886672fdc38d50dc7300348170
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-12 15:33:12 +0900

    success to change delete-btn image

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -3,4 +3,9 @@ class KeywordsController < ApplicationController
     @keyword = Keyword.new(keyword: params[:keyword][:keyword], user_id: current_user.id)
     @keyword.save
   end
+
+  def destroy
+    binding.pry
+    redirect_to "/users/#{current_user.id}/timeline"
+  end
 end
