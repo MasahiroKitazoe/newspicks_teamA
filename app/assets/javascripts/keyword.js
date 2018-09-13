d1ac84a41a90251d49aa542ea118818a31352e53
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-13 13:28:38 +0900

    tabMenuChage function

--- app/assets/javascripts/keyword.js
+++ app/assets/javascripts/keyword.js
@@ -1,21 +1,19 @@
 $(function() {
+  function tabStyleChenge(block, none) {
+    $('#keyword-class-' + block).css("color", "#4277FB");
+    $('#keyword-class-' + block).css("border-bottom", "2px solid #4277FB");
+    $('#keyword-class-' + block).css("font-weight", "bold");
+    $('#keyword-class-' + none).css("color", "#999");
+    $('#keyword-class-' + none).css("border-bottom", "2px solid #fff");
+    $('#keyword-class-' + none).css("font-weight", "normal");
+  }
   $('#keyword-class-news').on('click', function() {
-    $(this).css("color", "#4277FB");
-    $(this).css("border-bottom", "2px solid #4277FB");
-    $(this).css("font-weight", "bold");
-    $('#keyword-class-comment').css("color", "#999");
-    $('#keyword-class-comment').css("border-bottom", "2px solid #fff");
-    $('#keyword-class-comment').css("font-weight", "normal");
+    tabStyleChenge('news', 'comment');
     $('.keyword-wrapper__form__result__comments').css("display", "none");
     $('.keyword-wrapper__form__result__picks').css("display", "block");
   });
   $('#keyword-class-comment').on('click', function() {
-    $(this).css("color", "#4277FB");
-    $(this).css("border-bottom", "2px solid #4277FB");
-    $(this).css("font-weight", "bold");
-    $('#keyword-class-news').css("color", "#999");
-    $('#keyword-class-news').css("border-bottom", "2px solid #fff");
-    $('#keyword-class-news').css("font-weight", "normal");
+    tabStyleChenge('comment', 'news');
     $('.keyword-wrapper__form__result__picks').css("display", "none");
     $('.keyword-wrapper__form__result__comments').css("display", "block");
   });
