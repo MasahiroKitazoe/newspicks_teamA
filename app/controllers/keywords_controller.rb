225736c707147611e239c0fd6b89f1ab6203b638
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-14 14:24:24 +0900

    fix #create&destroy & not each, render

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -1,12 +1,26 @@
 class KeywordsController < ApplicationController
   def create
-    @keyword = current_user.keywords.build(keyword: params[:keyword][:keyword])
-    @keyword.save
+    @keyword = current_user.keywords.build(keyword_params)
+    respond_to do |format|
+      if @keyword.save
+        format.js
+      else
+        format.js
+      end
+    end
   end
 
   def destroy
     keyword = Keyword.find_by(keyword: params[:keyword][:keyword], user_id: current_user)
-    keyword.destroy
-    redirect_to "/users/#{current_user.id}/timeline"
+    if keyword.destroy
+      redirect_to controller: 'users', action: 'timeline'
+    else
+      render controller: 'users', action: 'timeline'
+    end
+  end
+
+  private
+  def keyword_params
+    params.require(:keyword).permit(:keyword)
   end
 end
