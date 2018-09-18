b0d02687389de08fed9a6e0a231b4e487ad1835b
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-18 19:39:23 +0900

    success to arrow_image_change

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -3,7 +3,10 @@ $(function() {
     autoplay: true,
     autoplaySpeed: 5000,
     speed: 1000,
-    // dots: true,
-    fade: true
+    prevArrow: '<img src="https://newspicks.com/images/slide_arrow_left_hover.b463fc1a.png" class="slick-prev">',
+    nextArrow: '<img src="https://newspicks.com/images/slide_arrow_right_hover.73006b69.png" class="slick-next">',
+    dots: true,
+    fade: true,
+    dotsClass: 'slide-dots'
   });
 });
