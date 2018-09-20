0c8ed5a691037efc9754d965dbf54fafc5c457b9
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-20 15:26:39 +0900

    prepare production env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,17 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
-#
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID_NP"],
-#     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY_NP"],
-#     region: 'ap-northeast-1'
-#   }
-#
-#   config.fog_directory  = 'newspicks-teama-image-bucket'
-#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/newspicks-teama-image-bucket'
-# end
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID_NP"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY_NP"],
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'newspicks-teama-image-bucket'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/newspicks-teama-image-bucket'
+end
