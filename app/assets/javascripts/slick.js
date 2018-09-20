e70b5ba1b7e07301c276fd7c47e573c1e2aeff9a
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-09-20 15:23:22 +0900

    fix responsive design in toppage

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -1,7 +1,7 @@
 $(function() {
   $('.content__eyecatch__main__slick').slick({
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 4000,
     speed: 1000,
     prevArrow: '<img src="https://newspicks.com/images/slide_arrow_left_hover.b463fc1a.png" class="slick-prev">',
     nextArrow: '<img src="https://newspicks.com/images/slide_arrow_right_hover.73006b69.png" class="slick-next">',
