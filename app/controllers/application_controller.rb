98d0b74305d4dcedebae72511a6d9dcec519373d
北添真広 <sas.surfer0jonny@gmail.com>
2018-10-04 15:23:42 +0900

    modify basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,6 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :company, :positon, :profile, :pro, :real_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :company, :positon, :profile, :pro, :real_name, :image])
